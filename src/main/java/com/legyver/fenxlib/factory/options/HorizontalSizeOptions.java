766cd8f0e6c6a2bf6d71d74ef0b154c10ba5e0cc
Arnold <Benjamin.Arnold@parexel.com>
2018-06-08 00:57:44 -0400

    removed unused size options classes

--- src/main/java/com/legyver/fenxlib/factory/options/HorizontalSizeOptions.java
+++
@@ -1,9 +0,0 @@
-package com.legyver.fenxlib.factory.options;
-
-public class HorizontalSizeOptions extends OrientatedSizeOption {
-
-	public HorizontalSizeOptions(double min, double pref, double max) {
-		super(min, pref, max);
-	}
-
-}
--- src/main/java/com/legyver/fenxlib/factory/options/VerticalSizeOptions.java
+++
@@ -1,9 +0,0 @@
-package com.legyver.fenxlib.factory.options;
-
-public class VerticalSizeOptions extends OrientatedSizeOption {
-
-	public VerticalSizeOptions(double min, double pref, double max) {
-		super(min, pref, max);
-	}
-
-}
