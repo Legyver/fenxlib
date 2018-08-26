e9e295e783f21499c06f1dbe60c6abbe06cb6bc2
benfarnold <benfarnold@yahoo.com>
2018-08-26 09:56:22 -0400

    GuidUtil.getGuid() now automatically assigns a GUID if one is not present

--- src/main/java/com/legyver/fenxlib/util/GuidUtil.java
+++ src/main/java/com/legyver/fenxlib/util/GuidUtil.java
@@ -7,12 +7,25 @@ public class GuidUtil {
 
 	private static final String GUID_KEY = "guidKey";
 
-	public static UUID getGuid(Node node) {
+	public static UUID getGuid(Node node, boolean init) {
+		UUID guid = getSetGuid(node);
+		if (guid == null && init) {
+			initGuid(node);
+			guid = getSetGuid(node);
+		}
+		return guid;
+	}
+
+	private static UUID getSetGuid(Node node) {
 		return (UUID) node.getProperties().get(GUID_KEY);
 	}
 
+	public static UUID getGuid(Node node) {
+		return getGuid(node, true);
+	}
+
 	public static void initGuid(Node node) {
-		if (getGuid(node) == null) {
+		if (getSetGuid(node) == null) {
 			node.getProperties().put(GUID_KEY, UUID.randomUUID());
 		}
 	}
