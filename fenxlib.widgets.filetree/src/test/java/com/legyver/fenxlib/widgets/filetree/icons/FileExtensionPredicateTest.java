2193d51d5ce7c9cd561ef135f254be34016dba96
Benjamin Arnold <benfarnold@yahoo.com>
2022-07-30 17:47:52 -0700

    Fix test

--- fenxlib.widgets.filetree/src/test/java/com/legyver/fenxlib/widgets/filetree/icons/FileExtensionPredicateTest.java
+++ fenxlib.widgets.filetree/src/test/java/com/legyver/fenxlib/widgets/filetree/icons/FileExtensionPredicateTest.java
@@ -10,14 +10,14 @@ public class FileExtensionPredicateTest {
 
     @Test
     public void singleExtension() {
-        Predicate<String> predicate = new FileExtensionPredicate(".gz");
+        Predicate<String> predicate = new FileExtensionPredicate("gunzip", ".gz");
         assertThat(predicate.test("name.gz")).isTrue();
         assertThat(predicate.test("nam.egz")).isFalse();
     }
 
     @Test
     public void orFileExtensions() {
-        Predicate<String> predicate = new FileExtensionPredicate(".gz|.7z|.zip");
+        Predicate<String> predicate = new FileExtensionPredicate("zip", ".gz|.7z|.zip");
         assertThat(predicate.test("name.gz")).isTrue();
         assertThat(predicate.test("nam.egz")).isFalse();
         assertThat(predicate.test("name.7z")).isTrue();
@@ -30,7 +30,7 @@ public class FileExtensionPredicateTest {
 
     @Test
     public void tarBalls() {
-        Predicate<String> predicate = new FileExtensionPredicate(".tar.gz");
+        Predicate<String> predicate = new FileExtensionPredicate("tarball", ".tar.gz");
         assertThat(predicate.test("name.tar.gz")).isTrue();
         assertThat(predicate.test("name.tar")).isFalse();
         assertThat(predicate.test("name.gz")).isFalse();
